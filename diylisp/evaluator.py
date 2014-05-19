264b4dc90a6af4cc31a6771b5ed6c0fb8840c573
Aneesh Roy <roy.aneesh@gmail.com>
2014-05-19 16:59:32 +0200

    Passed test: "The parameters of a `lambda` should be a list."

--- diylisp/evaluator.py
+++ diylisp/evaluator.py
@@ -54,6 +54,8 @@ def evaluate(ast, env):
         env.set(ast[1], evaluate(ast[2], env))
         return ""
     elif ast[0] == 'lambda':
+        if not is_list(ast[1]):
+            raise LispError('Expected list: %s' % ast[1])
         return Closure(env, ast[1], ast[2])
 
     raise LispError('Symbol Unknown: %s' % ast[0])
