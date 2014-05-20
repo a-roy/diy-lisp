07837334455cb30e62c2d71059e708808059c538
Aneesh Roy <roy.aneesh@gmail.com>
2014-05-20 19:06:16 +0200

    Changed keyword evaluation to use a dictionary and helper functions

--- diylisp/evaluator.py
+++ diylisp/evaluator.py
@@ -23,6 +23,43 @@ math_operators = {
         'mod' : operator.mod,
         '>' : operator.gt}
 
+def eval_quote(ast, env):
+    return ast[1]
+
+def eval_atom(ast, env):
+    return is_atom(evaluate(ast[1], env))
+
+def eval_eq(ast, env):
+    args = [evaluate(x, env) for x in ast[1:]]
+    return is_atom(args[0]) and args[0] == args[1]
+
+def eval_if(ast, env):
+    return evaluate(ast[2] if evaluate(ast[1], env) else ast[3], env)
+
+def eval_define(ast, env):
+    if len(ast) != 3:
+        raise LispError('Wrong number of arguments')
+    if not is_symbol(ast[1]):
+        raise LispError('non-symbol: %s' % unparse(ast[1]))
+    env.set(ast[1], evaluate(ast[2], env))
+    return ""
+
+def eval_lambda(ast, env):
+    if len(ast) != 3:
+        raise LispError('Wrong number of arguments')
+    if not is_list(ast[1]):
+        raise LispError('non-list: %s' % unparse(ast[1]))
+    return Closure(env, ast[1], ast[2])
+
+keywords = {
+        'quote' : eval_quote,
+        'atom' : eval_atom,
+        'eq' : eval_eq,
+        'if' : eval_if,
+        'define' : eval_define,
+        'lambda' : eval_lambda
+        }
+
 def evaluate(ast, env):
     """Evaluate an Abstract Syntax Tree in the specified environment."""
     if is_boolean(ast) or is_integer(ast):
@@ -32,36 +69,17 @@ def evaluate(ast, env):
 
     if not is_atom(ast[0]):
         ast[0] = evaluate(ast[0], env)
-    elif ast[0] == 'quote':
-        return ast[1]
-    elif ast[0] == 'atom':
-        return is_atom(evaluate(ast[1], env))
-    elif ast[0] == 'eq':
-        args = [evaluate(x, env) for x in ast[1:]]
-        return is_atom(args[0]) and args[0] == args[1]
-    elif is_symbol(ast[0]) and ast[0] in math_operators:
-        args = [evaluate(x, env) for x in ast[1:]]
-        if not (is_integer(args[0]) and is_integer(args[1])):
-            raise LispError('Arguments must be integers.')
-        return math_operators[ast[0]](args[0], args[1])
-    elif ast[0] == 'if':
-        return evaluate(ast[2] if evaluate(ast[1], env) else ast[3], env)
-    elif ast[0] == 'define':
-        if len(ast) != 3:
-            raise LispError('Wrong number of arguments')
-        if not is_symbol(ast[1]):
-            raise LispError('non-symbol: %s' % unparse(ast[1]))
-        env.set(ast[1], evaluate(ast[2], env))
-        return ""
-    elif ast[0] == 'lambda':
-        if len(ast) != 3:
-            raise LispError('Wrong number of arguments')
-        if not is_list(ast[1]):
-            raise LispError('non-list: %s' % unparse(ast[1]))
-        return Closure(env, ast[1], ast[2])
+    elif is_symbol(ast[0]):
+        if ast[0] in keywords:
+            return keywords[ast[0]](ast, env)
+        elif ast[0] in math_operators:
+            args = [evaluate(x, env) for x in ast[1:]]
+            if not (is_integer(args[0]) and is_integer(args[1])):
+                raise LispError('Arguments must be integers.')
+            return math_operators[ast[0]](args[0], args[1])
+        else:
+            ast[0] = env.lookup(ast[0])
 
-    if is_symbol(ast[0]):
-        ast[0] = env.lookup(ast[0])
     if is_closure(ast[0]):
         args = [evaluate(x, env) for x in ast[1:]]
         num_args = len(args)
